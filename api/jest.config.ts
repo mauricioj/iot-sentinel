da7eca70e20c0fc038173d3e3fb9fd902aa461e8
Mauricio De Grandi <mauricioj.br@gmail.com>
2026-03-23 16:06:42 -0300

    chore: bootstrap NestJS project with TypeScript, ESLint, Docker

---
+++ api/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s', '!**/*.module.ts', '!main.ts'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@common/(.*)$': '<rootDir>/common/$1',
+    '^@modules/(.*)$': '<rootDir>/$1',
+  },
+};
+
+export default config;
